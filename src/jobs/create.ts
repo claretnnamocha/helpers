7e9dfc68dc65946e1860e4b515fc5b0f6c061873
claretnnamocha <devclareo@gmail.com>
2022-06-26 22:51:18 +0100

    Update create.ts

--- src/jobs/create.ts
+++ src/jobs/create.ts
@@ -5,6 +5,6 @@ export const create = ({
   queueName,
   options = {},
 }: jobs.create): Bull.Queue => {
-  const {REDIS_URL = null} = process.env;
-  return new Bull(queueName, null, options);
+  const {REDIS_URL = null, REDIS_TLS_URL = null} = process.env;
+  return new Bull(queueName, REDIS_TLS_URL || REDIS_URL, options);
 };
